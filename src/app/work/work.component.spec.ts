8abd8d7d2b064ad52ea7afa074f04c013aababab
Lala <lalaklod@gmail.com>
2019-12-16 22:00:44 +0100

    stub works

--- src/app/work/work.component.spec.ts
+++ src/app/work/work.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, flush } from '@angular/core/testing';
 
 import { GoodWorkComponent } from './good-work/good-work.component';
 import { BadWorkComponent }  from './bad-work/bad-work.component';
@@ -6,9 +6,12 @@ import { BadWorkComponent }  from './bad-work/bad-work.component';
 import { WorkComponent } from './work.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { WorkRoutingModule, workRoutes } from './work-routing.module';
-import {Location} from "@angular/common";
+import {Location} from '@angular/common';
 import {fakeAsync, tick} from '@angular/core/testing';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
+import { AppComponent } from '../app.component';
+import { By } from '@angular/platform-browser';
+import { WorkingComponent } from '../working/working.component';
 fdescribe('Router: ChildrenApp', () => {
 
   let location: Location;
@@ -17,14 +20,18 @@ fdescribe('Router: ChildrenApp', () => {
 
 beforeEach(() => {
   TestBed.configureTestingModule({
-    imports: [RouterTestingModule.withRoutes(workRoutes)],
+    imports: [RouterTestingModule.withRoutes([
+  {path: 'goodwork', component: GoodWorkComponent},
+  {path: 'badwork', component: BadWorkComponent},
+  {path: 'working/:id', component: WorkingComponent}])],
     declarations: [
       WorkComponent,
       GoodWorkComponent,
-      BadWorkComponent
+      BadWorkComponent,
+      WorkingComponent
 
     ]
-  });
+  }).compileComponents();
 
   router = TestBed.get(Router);
   location = TestBed.get(Location);
@@ -32,14 +39,34 @@ beforeEach(() => {
   fixture = TestBed.createComponent(WorkComponent);
   router.initialNavigation();
 });
-it('navigate to "./goodwork" takes you to /good.work', fakeAsync(() => {
-  router.navigate(['goodwork']);
-  tick();
+beforeEach(fakeAsync(() => {
+  router = TestBed.get(Router);
+  location = TestBed.get(Location);
+  fixture = TestBed.createComponent(WorkComponent);
+  router.navigateByUrl('/');
+  advance();
+}));
+
+function advance(): void {
+  flush();
+  fixture.detectChanges();
+}
+it('navigate to "./goodwork" takes you to /goodwork', fakeAsync(() => {
+  const menu = fixture.debugElement.query(By.css('.goodwork'));
+  menu.triggerEventHandler('click', { button: 0 });
+  advance();
   expect(location.path()).toBe('/goodwork');
 }));
-it('navigate to "./badwork" takes you to /bad.work', fakeAsync(() => {
-  router.navigate(['badwork']);
-  tick();
+it('navigate to "./badwork" takes you to /badwork', fakeAsync(() => {
+  const menu = fixture.debugElement.query(By.css('.badwork'));
+  menu.triggerEventHandler('click', { button: 0 });
+  advance();
   expect(location.path()).toBe('/badwork');
+}));
+it('navigate to working with id', fakeAsync(() => {
+  const menu = fixture.debugElement.query(By.css('button'));
+  menu.triggerEventHandler('click', null);
+  advance();
+  expect(location.path()).toBe('/working/1');
 }));
 });
